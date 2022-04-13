ccb35dec60c7d30b0cb7beb634823406d231dc16
Jurre Vriezinga <j.vriezinga@gmail.com>
2022-04-13 12:02:46 +0200

    Use psr/log instead of symfony/var-dumper

--- src/Bayeux.php
+++ src/Bayeux.php
@@ -2,6 +2,7 @@
 
 namespace Skitlabs\Bayeux;
 
+use Psr\Log\LoggerInterface;
 use Skitlabs\Bayeux\Http\HttpClient;
 use Skitlabs\Bayeux\Message\Message;
 use Skitlabs\Bayeux\State\State;
@@ -11,13 +12,16 @@ use Skitlabs\Bayeux\State\StateHandshake;
 class Bayeux
 {
     private HttpClient $http;
+    private LoggerInterface $logger;
     private State $state;
+
     private array $channels = [];
     private string $clientId = '';
 
-    public function __construct(HttpClient $http)
+    public function __construct(HttpClient $http, LoggerInterface $logger)
     {
         $this->http = $http;
+        $this->logger = $logger;
         $this->state = new StateHandshake();
     }
 
@@ -43,11 +47,12 @@ class Bayeux
     {
         do {
             try {
-                dump(date("[Y-m-d H:i:s]") . ' ' . ($this->state::class));
+                $this->logger->info($this->state::class);
 
                 $this->state = $this->state->process($this);
             } catch (\Throwable $e) {
-                dump($e);
+                $this->logger->critical($e->getMessage());
+
                 $this->state = new StateDisconnected($e->getMessage());
             }
         } while (! $this->state->isDisconnected());
@@ -57,11 +62,11 @@ class Bayeux
     {
         $messages = array_map(fn (Message $message) => $message->setClientId($this->clientId)->asArray(), $messages);
 
-        dump(date("[Y-m-d H:i:s]") . ' ' . $url . ' ' . json_encode($messages, JSON_THROW_ON_ERROR));
+        $this->logger->debug($url, $messages);
 
         $response = $this->http->post($url, $messages);
 
-        dump(date("[Y-m-d H:i:s]") . ' ' . $url . ' ' . json_encode($response, JSON_THROW_ON_ERROR));
+        $this->logger->debug($url, $response);
 
         return $response;
     }
